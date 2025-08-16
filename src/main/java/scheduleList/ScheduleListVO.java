8a47e3901fc776eaa1dd1cbf5edfe75f8476d09b
영준 <ernmqq@gmail.com>
2025-08-16 17:43:11 +0900

    중간점검

--- src/main/java/scheduleList/ScheduleListVO.java
+++ src/main/java/scheduleList/ScheduleListVO.java
@@ -1,71 +1,80 @@
 package scheduleList;
 
+import java.sql.Timestamp;
+
 public class ScheduleListVO {
     private int scheduleId;
     private int movieId;
     private int roomId;
-    private int cinemaId;
-    private String startTime;
-    private String roomName;
+    private Timestamp startTime;
+    private String timeOnly;      // "12:00" 형태의 시간만
     private String movieTitle;
-    
-    public ScheduleListVO() {
-    }
-    
-    // Getter와 Setter 메서드들
+    private String roomName;
+    private String cinemaName;
+
+    public ScheduleListVO() {}
+
     public int getScheduleId() {
         return scheduleId;
     }
-    
+
     public void setScheduleId(int scheduleId) {
         this.scheduleId = scheduleId;
     }
-    
+
     public int getMovieId() {
         return movieId;
     }
-    
+
     public void setMovieId(int movieId) {
         this.movieId = movieId;
     }
-    
+
     public int getRoomId() {
         return roomId;
     }
-    
+
     public void setRoomId(int roomId) {
         this.roomId = roomId;
     }
-    
-    public int getCinemaId() {
-        return cinemaId;
-    }
-    
-    public void setCinemaId(int cinemaId) {
-        this.cinemaId = cinemaId;
-    }
-    
-    public String getStartTime() {
+
+    public Timestamp getStartTime() {
         return startTime;
     }
-    
-    public void setStartTime(String startTime) {
+
+    public void setStartTime(Timestamp startTime) {
         this.startTime = startTime;
     }
-    
-    public String getRoomName() {
-        return roomName;
+
+    public String getTimeOnly() {
+        return timeOnly;
     }
-    
-    public void setRoomName(String roomName) {
-        this.roomName = roomName;
+
+    public void setTimeOnly(String timeOnly) {
+        this.timeOnly = timeOnly;
     }
-    
+
     public String getMovieTitle() {
         return movieTitle;
     }
-    
+
     public void setMovieTitle(String movieTitle) {
         this.movieTitle = movieTitle;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public String getCinemaName() {
+        return cinemaName;
+    }
+
+    public void setCinemaName(String cinemaName) {
+        this.cinemaName = cinemaName;
     }
 }
---
+++ src/main/java/scheduleList/scheduleListDAO.java
@@ -0,0 +1,5 @@
+package scheduleList;
+
+public class scheduleListDAO {
+
+}
