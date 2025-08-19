b61e0ff161f343798fbb870b2d40c14644ceea7e
영준 <ernmqq@gmail.com>
2025-08-19 12:23:53 +0900

    Add files via upload

--- src/main/java/reserveList/ReserveListVO.java
+++ src/main/java/reserveList/ReserveListVO.java
@@ -1,18 +1,29 @@
 package reserveList;
 
+import java.sql.Date;
+import java.sql.Time;
 import java.sql.Timestamp;
 
 public class ReserveListVO {
-	public int reservationId;
-	public int userId;
-	public int scheduleId;
-	public int seatId;
-	public String movieTitle;
-	public String cinemaName;
-    public String roomName;
+    public int reservationId;
+    public int userId;
+    public int scheduleId;
+    public String movieTitle;
+    public String cinemaName;
     public Timestamp startTime;
-    public String seatLabel;
     public Integer price;
+    public Date showDate;     // 상영 날짜 추가
+    public Time showTime;     // 상영 시간 추가
+    
+    public String moviePoster;
+
+    public String getMoviePoster() {
+        return moviePoster;
+    }
+
+    public void setMoviePoster(String moviePoster) {
+        this.moviePoster = moviePoster;
+    }
 
     
     public int getReservationId() { return reservationId; }
@@ -24,8 +35,6 @@ public class ReserveListVO {
     public int getScheduleId() { return scheduleId; }
     public void setScheduleId(int scheduleId) { this.scheduleId = scheduleId; }
 
-    public int getSeatId() { return seatId; }
-    public void setSeatId(int seatId) { this.seatId = seatId; }
 
     public String getMovieTitle() { return movieTitle; }
     public void setMovieTitle(String movieTitle) { this.movieTitle = movieTitle; }
@@ -33,16 +42,17 @@ public class ReserveListVO {
     public String getCinemaName() { return cinemaName; }
     public void setCinemaName(String cinemaName) { this.cinemaName = cinemaName; }
 
-    public String getRoomName() { return roomName; }
-    public void setRoomName(String roomName) { this.roomName = roomName; }
 
     public Timestamp getStartTime() { return startTime; }
     public void setStartTime(Timestamp startTime) { this.startTime = startTime; }
 
-    public String getSeatLabel() { return seatLabel; }
-    public void setSeatLabel(String seatLabel) { this.seatLabel = seatLabel; }
 
     public Integer getPrice() { return price; }
     public void setPrice(Integer price) { this.price = price; }
 
+    public Date getShowDate() { return showDate; }
+    public void setShowDate(Date showDate) { this.showDate = showDate; }
+
+    public Time getShowTime() { return showTime; }
+    public void setShowTime(Time showTime) { this.showTime = showTime; }
 }
